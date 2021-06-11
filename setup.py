9a7b4cfb4ffd1565e99dc5c4c69947046cb81963
Zdenek Ruzicka <tramtara@seznam.cz>
2021-06-12 02:13:13 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ease_lonlat",
+    version="0.0.3",
+    author="Zdenek Ruzicka",
+    author_email="tramtara@seznam.cz",
+    description="To convert between LON, LAT coordinates and EASE(2) grid ROW, COL.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CzendaZdenda/ease_lonlat",
+    project_urls={
+        "Bug Tracker": "https://github.com/CzendaZdenda/ease_lonlat/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: GIS"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['pyproj'],
+    extras_require={
+        "testing":  ['pytest', 'numpy']}
+)
